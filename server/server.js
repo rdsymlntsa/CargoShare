e1261cb3377cf4f840453e6b08f047fcfc25425c
rdsymlntsa <rdsymlntsa@gmail.com>
2026-09-04 17:12:12 +0530

    feat: set up user model and mongoose config

--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({
-    message: "CargoShare API is running",
+    message: "CargoShare API is running"
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
