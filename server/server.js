899558dd7cc193123e01ab7768ed53d35a225dd5
rdsymlntsa <rdsymlntsa@gmail.com>
2026-09-05 20:31:58 +0530

    chore: deploy frontend configuration

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://YOUR-VERCEL-DOMAIN.vercel.app",
+    ],
     credentials: true,
   }),
 );
