d3c34e6b8961c4d1f9e76ca5546f2faf56d6e01d
rdsymlntsa <rdsymlntsa@gmail.com>
2026-09-05 17:27:18 +0530

    feat: configure frontend api client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux'
-
 function App() {
    const reduxState = useSelector((state) => state);
 
   console.log(reduxState);
 
   return (
-    <div className="flex bg-black min-h-screen items-center justify-center">
-      <h1 className="text-4xl text-teal-500 font-bold">
+    <div className="flex bg-contain  min-h-screen items-center justify-center" 
+     style={{ backgroundImage: "url('/image.jpeg')" }}>
+      <h1 className="text-6xl text-teal-500 font-bold">
         CargoShare
       </h1>
     </div>
